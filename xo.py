9b83b1ca68425ae74d05aecddfb9ef37c15a019a
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2024-12-12 18:22:38 +0100

    second commit

--- xo.py
+++ xo.py
@@ -12,13 +12,13 @@ def table():
     print(f" {indexes[2][0]}  |  {indexes[2][1]} | {indexes[2][2]}  ")
     print("    |    |    ")
 
-def changeValues(player_input):
+def changeValues(player_input, player):
     i = 0
     while i<len(indexes):
         j=0
         while j<len(indexes[i]):
             if player_input == indexes[i][j]:
-                indexes[i][j] = "x"
+                indexes[i][j] = player
                 break
             j += 1
         i += 1
@@ -26,8 +26,18 @@ def changeValues(player_input):
 
 
 
-
+turn = 1
 while True:
+    player = 'x'
+    if turn%2 == 0:
+        player = 'o'
+    
     table()
+    print('turn number: ', turn)
     player_input = input("Enter a valid number : ")
-    changeValues(player_input)
+    changeValues(player_input, player)
+    turn += 1
+
+# indexes[0][0] == indexes[0][1] == indexes[0][2]
+# indexes[1][0] == indexes[1][1] == indexes[1][2]
+# indexes[2][0] == indexes[2][1] == indexes[2][2]
