ba7b1cbaf1f3fae763ee0e952df3f7614d397b51
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2024-12-26 17:14:03 +0100

    snake game

--- pygame project/main.py
+++ pygame project/main.py
@@ -1,6 +1,6 @@
 # Example file showing a basic pygame "game loop"
 import pygame
-
+import random
 screen_width, screen_height = 1000, 500
 
 # pygame setup
@@ -9,33 +9,60 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 running = True
 x, y = 100, 100
+
+food_coordinates = (random.randint(50, 950),random.randint(50, 450)) # set
+
+def eat_food():
+    global x, y, food_coordinates
+    
+    if x+20 > food_coordinates[0] > x-20 and y+20 > food_coordinates[1] > y-20:
+        food_coordinates = (random.randint(50, 950),random.randint(50, 450))
+
+font = pygame.font.SysFont('Arial', 46)
+text = font.render('Game Over', True, (255,0,0))
+text2 = font.render('Press Q to quit the game', True, (255,0,0))
+game_over = False
 while running:
     # poll for events
+    
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     keys = pygame.key.get_pressed()
+    if not game_over:
+        if keys[pygame.K_RIGHT]:
+            x = x + 5
+        if keys[pygame.K_LEFT]:
+            x = x - 5
+        if keys[pygame.K_UP]:
+            y = y - 5
+        if keys[pygame.K_DOWN]:
+            y = y + 5
 
-    if keys[pygame.K_RIGHT]:
-        x = x + 5
-    if keys[pygame.K_LEFT]:
-        x = x - 5
-    if keys[pygame.K_UP]:
-        y = y - 5
-    if keys[pygame.K_DOWN]:
-        y = y + 5
 
-    if x<0 or y<0 or x>screen_width or y>screen_height :
-        running=False
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    # RENDER YOUR GAME HERE                      x    y
+    if x<0 or y<0 or x>screen_width or y>screen_height :
+        game_over=True
+        screen.blit(text, (300,250))
+        screen.blit(text2, (300,350))
+
+    if game_over and keys[pygame.K_q]:
+        running = False
+    # RENDER YOUR GAME HERE  
+    # snake                    
     pygame.draw.rect(screen,"green", pygame.Rect(x, y, 20, 20))
+    pygame.draw.circle(screen, "purple", food_coordinates, 10, 10)
+    
+    eat_food()
     
     # flip() the display to put your work on screen
+
+    pygame.display.update()
+
     pygame.display.flip()
 
     clock.tick(30)  # limits FPS to 60
